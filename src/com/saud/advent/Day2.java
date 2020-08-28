1dccb37fedfb061048a725a935aafcea22f0bf08
Sagar Saud <sagarsaudsagar@gmail.com>
2020-08-28 16:53:41 +0545

    :sparkles: day 2 part 2

--- src/com/saud/advent/Day2.java
+++ src/com/saud/advent/Day2.java
@@ -49,15 +49,38 @@ Here are the initial and final states of a few more small programs:
 1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99.
 Once you have a working computer, the first step is to restore the gravity assist program (your puzzle input) to the "1202 program alarm" state it had just before the last computer caught fire. To do this, before running the program, replace position 1 with the value 12 and replace position 2 with the value 2. What value is left at position 0 after the program halts?
 *
+*
+*
+*
+* --- Part Two ---
+"Good, the new computer seems to be working correctly! Keep it nearby during this mission - you'll probably use it again. Real Intcode computers support many more features than your new one, but we'll let you know what they are as you need them."
+
+"However, your current priority should be to complete your gravity assist around the Moon. For this mission to succeed, we should settle on some terminology for the parts you've already built."
+
+Intcode programs are given as a list of integers; these values are used as the initial state for the computer's memory. When you run an Intcode program, make sure to start by initializing memory to the program's values. A position in memory is called an address (for example, the first value in memory is at "address 0").
+
+Opcodes (like 1, 2, or 99) mark the beginning of an instruction. The values used immediately after an opcode, if any, are called the instruction's parameters. For example, in the instruction 1,2,3,4, 1 is the opcode; 2, 3, and 4 are the parameters. The instruction 99 contains only an opcode and has no parameters.
+
+The address of the current instruction is called the instruction pointer; it starts at 0. After an instruction finishes, the instruction pointer increases by the number of values in the instruction; until you add more instructions to the computer, this is always 4 (1 opcode + 3 parameters) for the add and multiply instructions. (The halt instruction would increase the instruction pointer by 1, but it halts the program instead.)
+
+"With terminology out of the way, we're ready to proceed. To complete the gravity assist, you need to determine what pair of inputs produces the output 19690720."
+
+The inputs should still be provided to the program by replacing the values at addresses 1 and 2, just like before. In this program, the value placed in address 1 is called the noun, and the value placed in address 2 is called the verb. Each of the two input values will be between 0 and 99, inclusive.
+
+Once the program has halted, its output is available at address 0, also just like before. Each time you try a pair of inputs, make sure you first reset the computer's memory to the values in the program (your puzzle input) - in other words, don't reuse memory from a previous attempt.
+
+Find the input noun and verb that cause the program to produce the output 19690720. What is 100 * noun + verb? (For example, if noun=12 and verb=2, the answer would be 1202.)
+
+Although it hasn't changed, you can still get your puzzle input.
 * */
 
 /**
  * @author SAUD on 8/27/2020.
  */
 public class Day2 {
-  private static int index = -1;
+  private int index = -1;
 
-  private static final int[] inputs =
+  private int[] inputs =
       new int[] {1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 9, 1, 19, 1, 5, 19, 23, 2, 9,
           23, 27, 1, 27, 5, 31, 2, 31, 13, 35, 1, 35, 9, 39, 1, 39, 10, 43, 2, 43, 9, 47, 1, 47, 5,
           51, 2, 13, 51, 55, 1, 9, 55, 59, 1, 5, 59, 63, 2, 6, 63, 67, 1, 5, 67, 71, 1, 6, 71, 75,
@@ -67,24 +90,44 @@ public class Day2 {
           14, 0, 0};
 
   public static void main(String[] args) {
-
+    int a, b =0;
     Day2 day2 = new Day2();
-    day2.repositionInputs();
-    day2.gravityAssistProgram();
+    boolean flag = false;
+    for (a = 0; a < 99; a++) {
+      for (b = 0; b < 99; b++) {
+
+        day2 = new Day2();
+        day2.repositionInputs(a, b);
+        day2.gravityAssistProgram();
+
+        if (day2.validateAnswer()) {
+          flag = true;
+          break;
+        }
+      }
+      if (flag) {
+        System.out.println(
+            "The value in 0 index is " + day2.inputs[0] + " for noun " + a + " and verb " + b);
+        break;
+      }
+
+    }
+
+    int result = ( (100 * a) + b);
+
     System.out.println("\n============================================"
-        + "\n The 1202 has alarmed Alarmed :" + inputs[0]
+        + "\n So the final result for 100 * noun + verb is : " + result
         + "\n============================================");
+  }
+
+  public boolean validateAnswer() {
+    return inputs[0] == 19690720;
   }
 
   private void gravityAssistProgram() {
     while (index < inputs.length - 3) {
       int code = getNextIndex();
       if (code == 99) {
-        System.out.println("################################\n"
-            + "Message to terminate the process."
-            + "\n=====================================\n"
-            + "Terminating........"
-            + "\n=====================================\n");
         break;
       } else {
         processAsInstructed(code);
@@ -116,8 +159,8 @@ public class Day2 {
     return n * m;
   }
 
-  private void repositionInputs() {
-    inputs[1] = 12;
-    inputs[2] = 2;
+  private void repositionInputs(int a, int b) {
+    inputs[1] = a;
+    inputs[2] = b;
   }
 }
